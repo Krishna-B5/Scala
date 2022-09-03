8f04d5bbd6fa47c4fe388e0ed2d167afaebe7886
Krishna-B5 <krishnab.05@gmail.com>
2022-09-03 11:42:02 +0530

    SparkSql

---
+++ SparkXML/src/main/scala/Pack/SparkSQL.scala
@@ -0,0 +1,34 @@
+package Pack
+
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import org.apache.spark.sql.SparkSession
+
+object SparkSQL {
+  
+  def main(args: Array[String]){
+    
+    println("===== Started =====")
+    println
+    
+    val conf = new SparkConf().setAppName("master").setMaster("local[*]")
+    val sc = new SparkContext(conf)
+    sc.setLogLevel("ERROR")
+    
+    val spark = SparkSession.builder().getOrCreate()
+    import spark.implicits._
+    
+    val df = spark.time(spark.read.format("jdbc")
+             .option("url", "jdbc:mysql://zeyodb.czvjr3tbbrsb.ap-south-1.rds.amazonaws.com/zeyodb")
+             .option("user", "root")
+             .option("password", "Aditya908")
+             .option("dbtable", "kgf")
+             .option("driver", "com.mysql.cj.jdbc.Driver")
+             .load()
+    )
+    
+    df.show()
+    df.printSchema()
+    print("====== Done ======")
+  }
+}
