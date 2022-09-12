eca83c16a4c282e0dafb19e14edd7e32c5523678
Krishna-B5 <krishnab.05@gmail.com>
2022-09-12 19:32:00 +0530

    2ndhighest

--- SparkDSL/src/main/scala/Pack/SecondHighest.scala
+++ SparkDSL/src/main/scala/Pack/SecondHighest.scala
@@ -10,35 +10,50 @@ import org.apache.spark.sql.types._
 import org.apache.spark.sql.expressions.Window
 
 object SecondHighest {
-  
-  def main(args: Array[String]){
-    
+
+  def main(args: Array[String]) {
+
     println("===== Started =====\n")
     val conf = new SparkConf().setAppName("Master").setMaster("local[*]")
     val sc = new SparkContext(conf)
     sc.setLogLevel("ERROR")
-    
+
     val spark = SparkSession.builder().getOrCreate()
     import spark.implicits._
-    
+
     val dtdf = spark.read.format("csv")
-                .option("header", "true")
-                .load("file:///c:/data/dtdata.csv")
-               
+                    .option("header", "true")
+                    .load("file:///c:/data/dtdata.csv")
+                //    .load("file:///c:/data/practise/dtdata.csv")
+
     dtdf.show()
-    
     println("===== Displaying the second highest salary =====\n")
-    
+ // for finding a second highest first data should be sorted done by window
     val secdf = Window
                 .partitionBy(col("dept"))
                 .orderBy(col("salary").desc)
-    
+
     val finaldf = dtdf.withColumn("SHSalary", dense_rank() over secdf)
-        //              .drop("SHSalary")
+              //        .drop("SHSalary")
+                        .orderBy("dept")
+              //        .filter("SHSalary=2")
+
+ /*   val secdf =  Window
+                .partitionBy()
+                .orderBy(col("salary"))
+
+    val finaldf = dtdf.withColumn("SHSalary", dense_rank() over secdf)
+                      .drop("SHSalary")
                       .orderBy("dept")
-   
-    finaldf.show()
-    println("\n ===== Done =====")
+             //         .filter("SHSalary=5") */
+                      
+    print("Enter a number to find the highest salary: ")
+    var n = scala.io.StdIn.readInt()     
+    println(s"The $n highest salary") 
     
+    finaldf.filter(s"SHSalary = $n").show() 
+    
+    println("\n ===== Done =====")
+
   }
 }
